332cdfcbe3e03753076da62cf5d3c83dcad696fd
Mateusz Juruś <mateuszjur@gmail.com>
2018-10-26 22:13:59 +0200

    lines on canvas are now arrows

--- main.js
+++ main.js
@@ -12,26 +12,77 @@ y = Number(y.substring(0,y.length-2));
 
 function getRandomPoint(x,y){
     //Generate random point within the canvas
-    
     return [
         Math.floor(Math.random()*x),
         Math.floor(Math.random()*y)
     ];
 }
-
+/*
 function createLine(){
     var cord = getRandomPoint(x,y);
     line.moveTo(x/2,y/2);
     line.lineTo(cord[0],cord[1]);
     line.stroke();
+    //
+    const alfa = 
+    //create arrow shape
+    arr1.moveTo(cord[0],cord[1]);
 }
+*/
+function createLine(fromx, fromy, tox, toy){
+    //variables to be used when creating the arrow
+    var ctx = c.getContext("2d");
+    var headlen = 10;
+
+    var angle = Math.atan2(toy-fromy,tox-fromx);
+
+    //starting path of the arrow from the start square to the end square and drawing the stroke
+    ctx.beginPath();
+    ctx.moveTo(fromx, fromy);
+    ctx.lineTo(tox, toy);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.stroke();
+
+    //starting a new path from the head of the arrow to one of the sides of the point
+    ctx.beginPath();
+    ctx.moveTo(tox, toy);
+    ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),toy-headlen*Math.sin(angle-Math.PI/7));
+
+    //path from the side point of the arrow, to the other side point
+    ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/7),toy-headlen*Math.sin(angle+Math.PI/7));
+
+    //path from the side point back to the tip of the arrow, and then again to the opposite side point
+    ctx.lineTo(tox, toy);
+    ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),toy-headlen*Math.sin(angle-Math.PI/7));
+
+    //draws the paths created above
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.fill();
+}
+
+
 
 function generateLines(){
     n = Math.floor(Math.random()*2)+3;
     for(i = 1; i <= n; i++){
-        console.log('xd')
-        createLine();
+        var cord = getRandomPoint(x,y);
+        createLine(x/2,y/2,cord[0],cord[1]);
     }
 }
 
-generateLines();
+//clear canvas
+function clear(){
+    ctx = c.getContext('2d');
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath();
+}
+
+//new canvas
+function newCanvas(){
+    clear();
+    generateLines();
+}
